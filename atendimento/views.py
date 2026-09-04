f46d26894c83d3c88acee0d6b682d9b8bbf15dd8
Thyago Antonio <thyagoa727@gmail.com>
2026-09-03 21:26:44 -0300

    Aula 03/09/2026

--- atendimento/views.py
+++ atendimento/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Servico
 
 def lista_servicos(request):
-    return HttpResponse("Página de Serviços do PetCare")
-# Create your views here.
+    servicos = Servico.objects.all()
+    return render(request, 'atendimento/servicos.html', {'servicos': servicos})
